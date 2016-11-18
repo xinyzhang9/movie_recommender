7f67227d7f62ec66089896bc11837be8df32b973
Xinyang <xinyzhang9@gmail.com>
2016-11-17 17:39:04 -0800

    one bug left

--- movie.js
+++ movie.js
@@ -5,11 +5,79 @@ var closeButton1 = document.querySelector('.close1');
 var closeButton2 = document.querySelector('.close2');
 var closeButton3 = document.querySelector('.close3');
 
+
 var refreshClickStream = Rx.Observable.fromEvent(refreshButton, 'click');
 var close1ClickStream = Rx.Observable.fromEvent(closeButton1, 'click');
 var close2ClickStream = Rx.Observable.fromEvent(closeButton2, 'click');
 var close3ClickStream = Rx.Observable.fromEvent(closeButton3, 'click');
 
+Storage.prototype.setObj = function(key, obj) {
+    return this.setItem(key, JSON.stringify(obj))
+}
+Storage.prototype.getObj = function(key) {
+    return JSON.parse(this.getItem(key))
+}
+
+// localStorage.setObj('movie_cart',[]);
+var cart;
+//init dropdown
+function initDropdown(){
+    //initialize localstorage
+    cart = localStorage.getObj('movie_cart');
+    if(!cart){
+        cart = [];
+        localStorage.setObj('movie_cart',[]);
+    }
+    console.log(cart);
+
+    var myDropdown = document.getElementById('myDropdown');
+    document.getElementById('drop_btn').textContent= 'Your Cart('+cart.length+')';
+    while (myDropdown.hasChildNodes()) {
+        myDropdown.removeChild(myDropdown.lastChild);
+    }
+    var cart_length = localStorage.getObj('movie_cart').length;
+    for(var i = 0; i < cart_length; i++){
+        var id = cart[i].split('#')[0];
+        var imgsrc = cart[i].split('#')[1];
+
+        var child = document.createElement('div');
+        var img = document.createElement('img');
+        // var imgEl = document.createElement('img');
+        img.src = 'https://image.tmdb.org/t/p/w500'+ imgsrc;
+        img.textContent = i;
+        img.id = id;
+        img.addEventListener('click',function(){
+            window.open('https://www.themoviedb.org/movie/'+this.id,'_blank');
+        })
+
+        var x = document.createElement('a');
+        x.textContent = 'X';
+        x.className = 'remove_cart_element';
+        x.id = id;
+        x.addEventListener('click',function(){
+            // console.log(this.id);
+            var this_id = this.id;
+            var newcart = cart.filter(function(c){
+                return !c.startsWith(this_id.toString());
+            })
+            localStorage.setObj('movie_cart',newcart);
+            //re-render
+            initDropdown();
+            return;
+        })
+        child.appendChild(img);
+        child.appendChild(x);
+        myDropdown.appendChild(child);
+
+    }
+}
+
+initDropdown();
+
+//dropdown function
+function dropFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
 
 var requestStream = refreshClickStream.startWith('startup click')
     .map(function(){
@@ -65,11 +133,22 @@ function renderSuggestion(suggestedMovie, selector) {
 
         var voteEL = suggestionEl.querySelector('.vote_average');
         voteEL.textContent = suggestedMovie.vote_average+'/10';
+
+        var addToCartEl = suggestionEl.querySelector('.addToCart');
+        addToCartEl.addEventListener("click", function(){
+            console.log(suggestedMovie.id);
+            if(cart.indexOf(suggestedMovie.id+'#'+suggestedMovie.poster_path) < 0){
+                cart.push(suggestedMovie.id+'#'+suggestedMovie.poster_path);
+                localStorage.setObj('movie_cart',cart);
+                initDropdown();
+            }
+        });
     }
 }
 
 suggestion1Stream.subscribe(function (suggestedMovie) {
     renderSuggestion(suggestedMovie, '.suggestion1');
+    console.log('stream 1',cart);
 });
 
 suggestion2Stream.subscribe(function (suggestedMovie) {
